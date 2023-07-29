e21d3bedb960c4a0dc391e61b12cc0c85b6fa4ac
kalleolumets <olumetskalle@gmail.com>
2023-07-29 12:02:05 +0300

    get ipfs service

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask,jsonify
+import urllib.request 
+import json
+import requests
+#from dotenv import load_dotenv
+
+proj_id = '2TEasvlIepRnGZwQWKGjy4PcRVS'
+proj_secret = '3f71ebe5ea462f4bb1c78f70bbb812ee'
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,3 +21,20 @@ def hello_json():
         'decimal':15
     }
     return jsonify(data)
+
+@app.route('/get_ipfs/<cid>')
+def get_ipfs(cid):
+    
+    #arg = "QmPmHk4MpNifLFxBzoCtwdiAHJSy8QKhRvN5eiaCYJKdkV"
+    arg =str(cid)
+    url = f"https://ipfs.infura.io:5001/api/v0/get?arg={arg}&archive=true"
+    
+    response = requests.post(url, auth=(proj_id, proj_secret))
+
+    return response.text
+
+
+        
+        
+        
+
